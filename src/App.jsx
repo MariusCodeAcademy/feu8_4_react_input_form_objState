3ae4066b270be447f4162f64bed6f40df8bb31ec
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-09 10:26:55 +0300

    Login jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import InputBlock from './components/InputBlock';
+import Login from './components/Login';
 import './style/App.css';
 
 export default function App() {
   return (
     <div className='container'>
       <h1>I am new App</h1>
-      <InputBlock />
+      {false && <InputBlock />}
+      <Login />
     </div>
   );
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,3 +1,18 @@
+export default function Login() {
+  return (
+    <form>
+      <div>
+        <label>Email:</label>
+        <input type='email' name='email' />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type='password' name='password' />
+      </div>
+      <button type='submit'>Login</button>
+    </form>
+  );
+}
 // sukurti komponenta
 // importuoti ji i App.jsx
 // jame buts forma
