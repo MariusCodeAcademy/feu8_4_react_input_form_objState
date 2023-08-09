fe8987d3ea908f98cbe82df10a04dd3818fd3a85
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-09 11:01:22 +0300

    success form state

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,13 @@
 import { useState } from 'react';
 
+const correctEmail = 'James@bond.com';
+const correctPass = 'secret123';
+
 export default function Login() {
   const [emailVal, setEmailVal] = useState('');
   const [passwVal, setPasswVal] = useState('');
+  const [formSuccess, setFormSuccess] = useState(false);
+
   /**
    * @param {InputEvent} event
    */
@@ -19,30 +24,54 @@ export default function Login() {
     // sustabdom puslapi nuo perkrovimo
     event.preventDefault();
     console.log('submit');
+    // daryti ka reikia
+    // atspausdinti ivestas reikmes:
+    console.log('ivesta email:', emailVal);
+    console.log('ivesta password:', passwVal);
+    // validuoti ar ne tusti laukai
+    if (emailVal.trim() === '' || passwVal.trim() === '') {
+      console.log('Abu laukai privalomi');
+      return;
+    }
+    // patikrinti ar sutampa email
+    // patikrinti ar sutampa password
+    // jei viskas gerai - sekmes atvejis
+    console.log('---Sekme---');
+    setFormSuccess(true);
   }
+  console.log('formSuccess ===', formSuccess);
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Email:</label>
-          <input
-            onChange={emailInput}
-            value={emailVal}
-            type='text'
-            name='email'
-          />
+      {!formSuccess && (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Email:</label>
+            <input
+              onChange={emailInput}
+              value={emailVal}
+              type='text'
+              name='email'
+            />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input
+              onChange={paswInput}
+              value={passwVal}
+              type='password'
+              name='password'
+            />
+          </div>
+          <button type='submit'>Login</button>
+        </form>
+      )}
+      {formSuccess && (
+        <div className='success'>
+          <h2>You have logged in succesfuly</h2>
+          <p>Welcome back to the website</p>
         </div>
-        <div>
-          <label>Password:</label>
-          <input
-            onChange={paswInput}
-            value={passwVal}
-            type='password'
-            name='password'
-          />
-        </div>
-        <button type='submit'>Login</button>
-      </form>
+      )}
+      <hr />
       <h3>Email: {emailVal}</h3>
       <h3>Password: {passwVal}</h3>
     </div>
