f8835b103e6f5ff2adbef6efc29b5d57b7c4e35e
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-09 09:26:04 +0300

    reset + InputBlock

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import InputBlock from './components/InputBlock';
+import './style/App.css';
+
 export default function App() {
   return (
     <div>
       <h1>I am new App</h1>
+      <InputBlock />
     </div>
   );
 }
--- src/components/InputBlock.jsx
+++ src/components/InputBlock.jsx
@@ -1,3 +1,12 @@
+export default function InputBlock() {
+  return (
+    <div>
+      <label htmlFor='input'>Input</label>
+      <input id='input' type='text' placeholder='enter here' />
+      <button>Enter</button>
+    </div>
+  );
+}
 // naujas komponentas
 
 // jame turi buti inputas ir mygtukas
