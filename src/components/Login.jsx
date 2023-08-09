8a23b2f977af27a5f20e34da196bbb8c201086a0
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-09 10:32:20 +0300

    email 2 way bind

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,33 @@
+import { useState } from 'react';
+
 export default function Login() {
+  const [emailVal, setEmailVal] = useState('');
+  /**
+   * @param {InputEvent} event
+   */
+  function emailInput(event) {
+    setEmailVal(event.target.value);
+  }
   return (
-    <form>
-      <div>
-        <label>Email:</label>
-        <input type='email' name='email' />
-      </div>
-      <div>
-        <label>Password:</label>
-        <input type='password' name='password' />
-      </div>
-      <button type='submit'>Login</button>
-    </form>
+    <div>
+      <form>
+        <div>
+          <label>Email:</label>
+          <input
+            onChange={emailInput}
+            value={emailVal}
+            type='text'
+            name='email'
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type='password' name='password' />
+        </div>
+        <button type='submit'>Login</button>
+      </form>
+      <h3>Email: {emailVal}</h3>
+    </div>
   );
 }
 // sukurti komponenta
