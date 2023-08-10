b3c764abdccefb5426c6a869ed91461b62e6afac
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-10 09:04:19 +0300

    setValue su fn

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,23 +1,40 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Counter() {
   const [value, setValue] = useState(0);
+
+  // useEffect(() => {}, []) - pasileis tik viena karta po komponento sugeneravimo
+  // () => {} - kas vyks
+  // [] => priklausomybiu masyvas
+
+  useEffect(() => {
+    console.log(
+      'turetu buti veiksmai kurie turi pasileisti pirma karta \n tik susikurus kompnentui'
+    );
+    plus2();
+  }, []);
 
   console.log('susikure');
 
   function plus2() {
     // setValue(value + 2);
-
     setTimeout(() => {
-      setValue(value + 2);
-    }, 3000);
+      // setValue(value + 5);
+      setValue((prevValue) => prevValue + 5);
+    }, 5000);
   }
   // negalima komponento viduje kviesti state keiciancios
   // funkcijos nes bus infinite loop
-  plus2();
+  // plus2();
 
   function plusValue() {
-    setValue(value + 1);
+    // setValue(value + 1);
+    // kai reiksme priklauso nuo pries tai buvusios
+    // atnaujinti reiktu su funkcija
+    setValue((prevValue) => prevValue + 1);
+    // setValue((prevValue) => prevValue + 1);
+    // setValue((prevValue) => prevValue + 1);
+    // setValue((prevValue) => prevValue + 1);
   }
 
   return (
