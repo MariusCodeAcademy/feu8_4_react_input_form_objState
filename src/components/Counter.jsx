2e0b6bc5650896c0d1bb2c1dada38ddef9fa8293
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-08-10 08:44:38 +0300

    the problem

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,6 +3,19 @@ import { useState } from 'react';
 export default function Counter() {
   const [value, setValue] = useState(0);
 
+  console.log('susikure');
+
+  function plus2() {
+    // setValue(value + 2);
+
+    setTimeout(() => {
+      setValue(value + 2);
+    }, 3000);
+  }
+  // negalima komponento viduje kviesti state keiciancios
+  // funkcijos nes bus infinite loop
+  plus2();
+
   function plusValue() {
     setValue(value + 1);
   }
